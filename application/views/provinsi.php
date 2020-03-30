018c446445ebd8cc981c161a2f4c81902650318e
fiki cahyanto <fikicahyanto96@gmail.com>
2020-03-30 08:39:18 +0700

    database update

--- application/views/provinsi.php
+++ application/views/provinsi.php
@@ -28,7 +28,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <a class="navbar-brand" href="index.html">COVID - 19</a><button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button
+            <a class="navbar-brand" href="<?php echo base_url().'dashboard'?>">COVID - 19</a><button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button
             ><!-- Navbar Search-->
             <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
             </form>
